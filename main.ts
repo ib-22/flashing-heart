4d9bcb17bc1b97d3787e50c2b4522bcde8d84c9c
ib-22 <154820729+ib-22@users.noreply.github.com>
2023-12-25 18:34:16 +0300

    Mise à jour pxt.json, main.blocks, main.ts, pxt.json, main.py

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
 input.onButtonPressed(Button.A, function () {
-	
+    basic.showNumber(1 * 2)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(1 * 1)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(1 + 2)
 })
